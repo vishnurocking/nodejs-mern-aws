b0252fba9cf8d6545d081270d762f2616cabe8d3
Vishnu S <vishnuitsrocking@gmail.com>
2024-07-24 19:26:02 +0530

    second commit

--- mongoC.js
+++ mongoC.js
@@ -1,13 +1,14 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+
+const connectionString = `mongodb+srv://vishnu:${password}@devcluster.cc4c2kr.mongodb.net/?retryWrites=true&w=majority`; // cluster url
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
 let db = conn.db("integration_ninjas");
