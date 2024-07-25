9dc7c8adc22de3c229d0ea9024adcc501e2c1099
Vishnu S <vishnuitsrocking@gmail.com>
2024-07-25 11:28:33 +0530

    Updated CRUD & UI

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import { ObjectId } from "mongodb";
 import db from "./mongoC.js";
 
 const port = 4000;
@@ -9,14 +9,11 @@ const app = express();
 app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
-
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
-// Parses the text as url encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Parses the text as json.
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
@@ -28,17 +25,31 @@ app.post("/addUser", async (req, res) => {
   let newDocument = req.body;
   newDocument.date = new Date();
   let result = await collection.insertOne(newDocument);
-  console.log("rreq" + req.body);
   res.send(result).status(204);
 });
 
 app.get("/getUsers", async (req, res) => {
   let collection = await db.collection("users");
-  let results = await collection
-    .find({})
-
-    .toArray();
+  let results = await collection.find({}).toArray();
   res.send(results).status(200);
+});
+
+app.put("/editUser/:id", async (req, res) => {
+  const userId = req.params.id;
+  const updates = req.body;
+  let collection = await db.collection("users");
+  let result = await collection.updateOne(
+    { _id: new ObjectId(userId) },
+    { $set: updates }
+  );
+  res.send(result).status(200);
+});
+
+app.delete("/deleteUser/:id", async (req, res) => {
+  const userId = req.params.id;
+  let collection = await db.collection("users");
+  let result = await collection.deleteOne({ _id: new ObjectId(userId) });
+  res.send(result).status(200);
 });
 
 app.listen(port, function () {
